09355d6a74be0a2e459c258ab7dc9aa674ccd991
Phuc <Phuc.Nguyen2@metropolia.fi>
2016-07-25 12:11:02 +0700

    tenth

---
+++ iClinic Online System/DanhSachDichVu.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DanhSachDichVu.aspx.cs" Inherits="iClinic_Online_System.DanhSachDichVu" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="grid" runat="server" AutoGenerateColumns="false"
+        OnRowDataBound="GridView1_RowDataBound" OnSelectedIndexChanged = "OnSelectedIndexChanged">
+        <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+ 				<Columns>
+					<asp:BoundField DataField="PAT_SER_ID" HeaderText="MA DICH VU" />
+					<asp:BoundField DataField="QUANTITY" HeaderText="SO LUONG" />
+					<asp:BoundField DataField="PRICE" HeaderText="DON GIA" />
+                    <asp:ButtonField Text="Click" CommandName="Select" ItemStyle-Width="30"  />
+				</Columns>
+       </asp:GridView>
+        <br />
+        <asp:Label ID="msg" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ iClinic Online System/DanhSachDichVu.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace iClinic_Online_System
+{
+    public partial class DanhSachDichVu : System.Web.UI.Page
+    {
+        private SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=iClinicDb;User ID=sa;Password=ThanhTruc1208");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            string sql = "select PAT_SER_ID, QUANTITY, PRICE from ServiceInfo";
+            connection.Open();
+            SqlCommand command = new SqlCommand(sql, connection);
+            adapter.SelectCommand = command;
+            adapter.Fill(ds);
+            adapter.Dispose();
+            command.Dispose();
+            connection.Close();
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                grid.DataSource = ds;
+                grid.DataBind();
+
+            }
+            else
+            {
+                ds.Tables[0].Rows.Add(ds.Tables[0].NewRow());
+                grid.DataSource = ds;
+                grid.DataBind();
+            }
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                e.Row.Attributes["onmouseover"] = "this.style.backgroundColor='aquamarine';";
+                e.Row.Attributes["onmouseout"] = "this.style.backgroundColor='white';";
+                e.Row.ToolTip = "Click last column for selecting this row.";
+            }
+        }
+
+        protected void OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            string pName = grid.SelectedRow.Cells[1].Text;
+            msg.Text = "<b>Publisher Name  &nbsp;:&nbsp;&nbsp;   " + pName + "</b>";
+        }
+
+    }
+}
